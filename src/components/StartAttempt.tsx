fbc31b2c7246f08fc11c280fe08c30a12d2a488c
okoryem <okoryem@udel.edu>
2025-03-07 15:31:22 -0500

    Copied over StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,35 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [inProgress, changeProgress] = useState<boolean>(false);
+
+    function start(): void {
+        let newAttempts = attempts - 1;
+        changeProgress(true);
+        setAttempts(newAttempts);
+    }
+
+    function increase(): void {
+        let newAttempts = attempts + 1;
+        setAttempts(newAttempts);
+    }
+
+    return (
+        <div>
+            <Button onClick={start} disabled={inProgress || attempts === 0}>
+                Start Quiz
+            </Button>
+            <Button
+                onClick={() => changeProgress(false)}
+                disabled={!inProgress}
+            >
+                Stop Quiz
+            </Button>
+            <Button onClick={increase} disabled={inProgress}>
+                Mulligan
+            </Button>
+            <div>{attempts}</div>
+        </div>
+    );
 }
